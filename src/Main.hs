98822f38c4b58defc6eab67be14c7c9f5d99db3e
Jan Hrček <jhrcek@globalwebindex.com>
2019-08-21 05:13:23 +0200

    Try out SMTLib

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import SMTLib2
+import SMTLib2.Core
+import SMTLib2.Int
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main =
+  print $ pp eightQueensScript
+
+eightQueensScript :: Script
+eightQueensScript = Script
+    [ CmdDeclareFun "f" [tInt, tInt] tBool
+    ]
